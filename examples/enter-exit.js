919fc74398f32d5f4979047c2ff451f3c64a954d
Devon Ho <devonho@msn.com>
2017-03-09 01:23:51 -0800

    added put to Things

--- examples/enter-exit.js
+++ examples/enter-exit.js
@@ -7,12 +7,64 @@
   based on code provided by: Mattias Ask (http://www.dittlof.com)
 */
 var noble = require('../index');
+var request = require('request');
+
 
 var RSSI_THRESHOLD    = -90;
 var EXIT_GRACE_PERIOD = 2000; // milliseconds
 
 var inRange = [];
 
+function sendRSSI(id,rssi) {
+
+    var thingsurl = 'https://things.apps.bosch-iot-cloud.com/api/1/things/' + encodeURIComponent(':') + id + '/features';
+
+    //console.log(thingsurl);
+
+    var options = {
+	url : thingsurl,
+	headers: {
+	    'x-cr-api-token':'ba7b6506558742e99f66dcd722cbdf42',
+	    'Authorization':'Basic ZGhkZW1vOlBhc3MxMjM0JA=='
+	}
+    }
+
+    function callback(error, response, body) {
+	if(!error && response.statusCode == 200){
+	    var info = JSON.parse(body);
+	    //console.log(body)
+	    //console.log(info.scanner.properties.rssi1);
+	    //console.log(info.scanner.properties.rssi2);
+	    //console.log(info.scanner.properties.rssi3);	    
+
+	    info.scanner.properties.rssi3 = rssi;
+	    
+	    options.body = info
+	    options.json = true
+	    
+	    request.put(options, function(error2,response2,body2) {
+
+		console.log('PUT: ' + response2.statusCode + ' RSSI: ' + rssi);
+
+	    });
+	}
+    }
+    
+    request.get(options, callback);
+}
+
+//sendRSSI('6eb94033-8713-40c3-a9a3-8ea327f93362','bar');
+
+noble.on('discover', function(peripheral) {
+
+    if(peripheral.id == 'fdcedea2fac4')
+    {
+	//console.log('ID: ' + peripheral.id + ' RSSI: ' + peripheral.rssi + ' NAME:' + peripheral.advertisement.localName);
+	sendRSSI('6eb94033-8713-40c3-a9a3-8ea327f93362',peripheral.rssi);
+    }
+})
+
+/*
 noble.on('discover', function(peripheral) {
   if (peripheral.rssi < RSSI_THRESHOLD) {
     // ignore
@@ -44,6 +96,9 @@ setInterval(function() {
     }
   }
 }, EXIT_GRACE_PERIOD / 2);
+*/
+
+
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
